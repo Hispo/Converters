a393c8ceb8f39509d588ad77f4bdca60c443955c
Edson F <Edson F@Edson>
2016-07-24 15:52:47 -0300

    Converters programs.

---
+++ Temperature Converter/src/_LightYearsTest.java
@@ -0,0 +1,13 @@
+
+
+public class _LightYearsTest {
+
+	public static void main(String[] args) {
+
+		_LightYears _lightYears = new _LightYears();
+		_lightYears.input();
+		_lightYears.setValueLightYears();
+		_lightYears.calculationLightYears();
+		_lightYears.message();
+	}
+}
